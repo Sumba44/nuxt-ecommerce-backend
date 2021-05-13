2b5e729d3b186845edeb46d076fa17dd654eebd5
Sumba44 <robokona@gmail.com>
2021-05-13 20:27:27 +0200

    upload / resize / delete original

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,5 +1,9 @@
 var multer = require("multer");
+const sharp = require("sharp");
+const fs = require("fs");
+
 var re = /(?:\.([^.]+))?$/;
+var fileName = "";
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/images");
@@ -7,6 +11,7 @@ var storage = multer.diskStorage({
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, file.fieldname + "-" + uniqueSuffix + "." + re.exec(file.originalname)[1]);
+    fileName = file.fieldname + "-" + uniqueSuffix + "." + re.exec(file.originalname)[1];
   }
 });
 var upload = multer({
@@ -41,8 +46,31 @@ exports.singleFile = (req, res, err) => {
       if (req.file == undefined) {
         res.status(400).send("No file selected");
       } else {
-        console.log("done!");
-        res.status(200).send("Upload OK");
+        // console.log(fileName);
+        // res.status(200).send("Upload OK");
+
+        // resize image
+        sharp(req.file.path)
+          .rotate()
+          .resize(1280)
+          .jpeg({ quality: 75 })
+          .toFile("./public/images/resized-" + fileName)
+          .then(data => {
+            
+            // delete original file
+            fs.unlink("./public/images/" + fileName, err => {
+              if (err) {
+                console.error(err);
+                res.status(500).send(err);
+                return;
+              }
+              res.status(200).send("Upload & Resize OK");
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).send(err);
+          });
       }
     }
   });
