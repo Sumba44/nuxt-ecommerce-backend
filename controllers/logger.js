210f9a88fc6fde4b2daefc7afbee271c8cf70b90
Sumba44 <robokona@gmail.com>
2021-05-14 14:57:42 +0200

    logs into /logs folder

--- controllers/logger.js
+++ controllers/logger.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 const helper = require("./helpers");
 
 exports.log = (type, log, err) => {
-  const logFile = fs.createWriteStream("X:/backend-" + helper.yyyymmdd() + ".log", {
+  const logFile = fs.createWriteStream("./logs/backend-" + helper.yyyymmdd() + ".log", {
     flags: "a"
   });
 
